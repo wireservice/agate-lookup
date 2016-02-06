87f8ba6d60c0b6c5fb4437f9560df21be65370b5
Christopher Groskopf <chrisgroskopf@gmail.com>
2016-02-06 12:16:45 -0600

    Add docstrings.

--- agatelookup/source.py
+++ agatelookup/source.py
@@ -4,7 +4,6 @@ import agate
 import agateremote
 import random
 import requests
-import six
 import yaml
 
 agateremote.patch()
@@ -12,6 +11,9 @@ agateremote.patch()
 # TKTK: host file somewhere we won't need cache busters
 
 def default_table_url_func(root, keys, value, version=None):
+    """
+    Default function for converting lookup table data into a table URL.
+    """
     if isinstance(keys, (list, tuple)):
         keys = '/'.join(keys)
 
@@ -25,6 +27,9 @@ def default_table_url_func(root, keys, value, version=None):
     return url
 
 def default_metadata_url_func(root, keys, value, version=None):
+    """
+    Default method of converting lookup table data into a metadata file url.
+    """
     if isinstance(keys, (list, tuple)):
         keys = '/'.join(keys)
 
@@ -38,6 +43,11 @@ def default_metadata_url_func(root, keys, value, version=None):
     return url
 
 def make_type_tester(meta):
+    """
+    Uses parsed lookup table metadata to create a :class:`.agate.TypeTester`
+    that will always use correct types for the table columns. (And avoid
+    the overhead of type inference.)
+    """
     force = {}
 
     for k, v in meta['columns'].items():
@@ -47,7 +57,21 @@ def make_type_tester(meta):
 
 class Source(object):
     """
-    TKTK
+    A reference to an archive of lookup tables. This is a remote location with
+    lookup table and metadata files at a known path structure.
+
+    :param root:
+        The root URL to prefix all data and metadata paths.
+    :param table_url_func:
+        A function that takes table metadata and returns an absolute URL to
+        that table's data file. See :func:`default_table_url_func`.
+    :param metadata_url_func:
+        A function that takes table metadata and returns an absolute URL to
+        that table's data file. See :func:`default_metadata_url_func`.
+    :param callback:
+        A function that translates the remote data table into an agate table.
+        Typically this is :meth:`.agate.Table.from_csv`, but it could also be
+        :meth:`.agate.Table.from_json` or a method provided by an extension.
     """
     def __init__(self, root='https://github.com/onyxfish/lookup/raw/master', table_url_func=default_table_url_func, metadata_url_func=default_metadata_url_func, callback=agate.Table.from_csv):
         self._root = root
@@ -56,6 +80,11 @@ class Source(object):
         self._callback = callback
 
     def get_metadata(self, keys, value, version=None):
+        """
+        Fetches metadata related to a specific lookup table.
+
+        See :meth:`Source.get_table` for parameter details.
+        """
         url = self._metadata_url_func(self._root, keys, value, version)
         r = requests.get(url)
 
@@ -64,6 +93,21 @@ class Source(object):
         return yaml.load(r.text)
 
     def get_table(self, keys, value, version=None):
+        """
+        Fetches and creates and agate table from a specified lookup table.
+
+        :param keys:
+            Either a single string or a sequence of keys that identify the
+            "left side" of the table. For example :code:`'fips'` or
+            :code:`['city', 'year']`.
+        :param value:
+            The value that is being looked up from the given keys. For example
+            :code:`'state'` or :code:`'population'`.
+        :param version:
+            An optional version of the given lookup, if more than one exists.
+            For instance :code:`'2007'` for the 2007 edition of the NAICS codes
+            or :code:`'2012'` for the 2012 version.
+        """
         meta = self.get_metadata(keys, value, version)
         tester = make_type_tester(meta)
 
--- agatelookup/table_lookup.py
+++ agatelookup/table_lookup.py
@@ -12,7 +12,27 @@ agateremote.patch()
 class TableLookup(object):
     def lookup(self, source, source_keys, value, table_keys=None, version=None):
         """
-        TKTK
+        Fetch a lookup table from the remote source, matches it this table by
+        its key columns, appends the value column and returns a new table
+        instance.
+
+        :param source:
+            An instance of :class:`.Source` that defines where lookup tables
+            are located.
+        :param source_keys:
+            The key (string) or keys (sequence) in the lookup table to be
+            matched. For example :code:`'naics'` or :code:`['city', 'year']`.
+        :param value:
+            The value that is being looked up. For example :code:`'description'`
+            or :code:`'population'`. This is the column that will be appended.
+        :param table_keys:
+            The keys in this table to match to the lookup table. This defaults
+            the same values specified for `source_keys`, so it only needs
+            to be specified if the column names in this table don't match.
+        :param version:
+            An optional version of the lookup to use, if more than one exists.
+            For instance :code:`'2007'` for the 2007 edition of the NAICS codes
+            or :code:`'2012'` for the 2012 version.
         """
         table = source.get_table(source_keys, value, version)
 
